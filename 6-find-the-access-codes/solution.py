fa5081d19a95e8537f7dc9bad2ec643291bba3ca
Darren Zhao <cyberelfd@gmail.com>
2021-09-17 00:08:29 +0800

    working on request 6

---
+++ 6-find-the-access-codes/solution.py
@@ -0,0 +1,11 @@
+def solution(l):
+    """
+    :type l: int[]
+    :rtype: int
+    """
+    
+
+if __name__=="__main__":
+    print(solution(12))
+    print(solution(15324))
+    print(solution(999))
