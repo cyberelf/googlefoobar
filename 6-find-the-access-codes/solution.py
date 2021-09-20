3f2d06e33d4e2b9b09c796da8d42954bd3de78cb
Darren Zhao <cyberelfd@gmail.com>
2021-09-21 00:32:45 +0800

    initial solution for request 6

--- 6-find-the-access-codes/solution.py
+++ 6-find-the-access-codes/solution.py
@@ -3,9 +3,17 @@ def solution(l):
     :type l: int[]
     :rtype: int
     """
-    
+    devide_counts = []
+    triples = 0
+    for i in range(0, len(l)):
+        devide_counts.append(0)
+        for j in range(i-1,-1,-1):
+            if l[i] % l[j] == 0:
+                devide_counts[-1] += 1
+                triples += devide_counts[j]
+    return triples
 
 if __name__=="__main__":
-    print(solution(12))
-    print(solution(15324))
-    print(solution(999))
+    print(solution([1, 2, 3, 4, 5, 6]))
+    print(solution([1, 2, 3, 4, 5, 10]))
+    print(solution([1, 1, 1, 1]))
